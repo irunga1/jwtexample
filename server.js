ad43b50f6c87cc93eb6b29a7ae30d6029ffebee1
@irunga1 <irunga1@yahoo.com>
2026-05-10 14:13:11 -0600

    Agregando cors para permitir entre distintos  sitios

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const express  = require('express');
 const SECRET = process.env.SECRET_KEY;
 const USER = { username: 'admin', password: '1234' };
 
 
+
 app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/status",(req,res) => {
@@ -21,7 +25,12 @@ app.post("/login",(req,res) => {
     console.log(USER);
     if(username == USER.username && password == USER.password){
         const token = jwt.sign({username},SECRET,{expiresIn:"1h"});
-        res.json({token});
+        let data1 = {
+            username:username,
+            password:password
+        }
+        console.log(data1);
+        res.json({token,data1});
     }else{
         res.status(401).json({message:"Invalid credentials",user:username,pass:password});
     }
