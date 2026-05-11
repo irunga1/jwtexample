01a8a772388cf4cacaf997cf1925046e0d813600
@irunga1 <irunga1@yahoo.com>
2026-05-10 20:02:32 -0600

    Agregando cors para permitir entre distintos  sitios

--- server.js
+++ server.js
@@ -1,55 +1,46 @@
-require('dotenv').config();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const express  = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 const SECRET = process.env.SECRET_KEY;
 const USER = { username: 'admin', password: '1234' };
 
+const app = express();
 
-
-app = express();
-
+// habilitar CORS para todas las rutas
 app.use(cors());
 app.use(express.json());
 
 app.get("/status",(req,res) => {
-    let server ={
-        status:"ok",
-        desc:"Server Runing"
-    }
-    res.json(server)
+    res.json({ status:"ok", desc:"Server Running" });
 });
 
 app.post("/login",(req,res) => {
     const {username,password} = req.body;
-    console.log(USER);
+    console.log(req.body);
     if(username == USER.username && password == USER.password){
         const token = jwt.sign({username},SECRET,{expiresIn:"1h"});
-        let data1 = {
-            username:username,
-            password:password
-        }
-        console.log(data1);
-        res.json({token,data1});
+        let status={"status":"ok","desc":"Login successful", "token":token};
+        res.json({status});
     }else{
-        res.status(401).json({message:"Invalid credentials",user:username,pass:password});
+        res.status(401).json({message:"Invalid credentials"});
     }
-})
+});
 
 app.get("/perfil",(req,res) => {
     const token = req.headers.authorization;
     if(!token){
-        return res.status(401).json({message:"No token provided",token:token});
+        return res.status(401).json({message:"No token provided"});
     }
     try {
         const decoded = jwt.verify(token,SECRET);
-        res.json({message:"Perfil data", user:decoded.username,"token":token});
+        res.json({message:"Perfil data", user:decoded.username, token});
     } catch (error) {
-        res.status(401).json({message:"Invalid token",token:token});
+        res.status(401).json({message:"Invalid token"});
     }
 });
 
-
-app.listen(3001, (params) => {
+app.listen(3001, () => {
     console.log("server start");
 });
